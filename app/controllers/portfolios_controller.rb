8573970f9439184559898ca5390672f8f1c5a586
Hugo Deveze <devezehugo@gmail.com>
2025-02-15 11:16:38 +0100

    branche portfolios

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,41 @@
+class PortfoliosController < ApplicationController
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = current_user.portfolios.new(portfolio_params)
+    @portfolio.save
+    redirect_to portfolio_path(@portfolio)
+  end
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.delete
+    redirect_to portfolios_path
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.update(portfolio_params)
+    redirect_to portfolio_path(@portfolio)
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :tags, :price_per_day)
+  end
+end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,7 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
+  TAG_OPTIONS = ["rock","pop","pop","jazz","classique"]
+
+  # Validation pour s'assurer que le tag est inclus dans la liste prédéfinie
+  validates :tags, inclusion: { in: TAG_OPTIONS }
 end
