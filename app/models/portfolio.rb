771b3fa6146c210923087ab93fcd14c947ebfc7d
Hugo Deveze <devezehugo@gmail.com>
2025-02-22 10:40:20 +0100

    Edit navbar

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,8 +4,11 @@ class Portfolio < ApplicationRecord
   has_many :portfolio_songs, dependent: :destroy
   has_many :songs, through: :portfolio_songs
   has_one_attached :photo
+
   TAG_OPTIONS = ["Pop", "Rock", "Hip-Hop", "Jazz", "Classical", "Electronic", "Reggae", "Country", "Blues", "Metal"]
 
-  # Validation pour s'assurer que le tag est inclus dans la liste prédéfinie
-  validates :tags, inclusion: { in: TAG_OPTIONS }
+  validates :title, presence: true
+  validates :tags, presence: true, inclusion: { in: TAG_OPTIONS }
+  validates :photo, presence: true
+  validates :price_per_day, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
